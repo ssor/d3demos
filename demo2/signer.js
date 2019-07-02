e9aa4cf0ce2e5d7d27dd5c12a159fd0f2a93043f
zhangquanzhi <zhangquanzhi@jd.com>
2019-07-02 14:50:23 +0800

    v1

---
+++ demo2/signer.js
@@ -0,0 +1,12 @@
+
+function Signer(name, x, y) {
+    let signer = {
+        name: name,
+        x: x,
+        y: y,
+        // width: width,
+        // height: height,
+    }
+
+    return signer
+}
